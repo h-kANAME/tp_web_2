09e8459710ffbe2c1a7a40ef33cb1b38a5241c3b
h-kaname <luis--ao@hotmail.com>
2020-09-13 19:45:26 -0300

    added sub nvar-center

--- inc/header.php
+++ inc/header.php
@@ -12,8 +12,10 @@
 		integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 	<link rel="stylesheet" href="styles/styles.css">
 </head>
+<!-- library -->
 
 <header>
+
 	<nav class="navbar-expand-lg navbar navbar-dark bg-secondary">
 		<a href="index.php"><img src="img/logo.png" alt=""> </a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
@@ -23,23 +25,31 @@
 
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav mr-auto">
-				<li class="nav-item active">
-					<a class="nav-link" href="hardware.php">Hardware</a>
-				</li>
-				<li class="nav-item active">
-					<a class="nav-link" href="software.php">Software</a>
-				</li>
-				<li class="nav-item active">
-					<a class="nav-link" href="#">Perifericos</a>
-				</li>
+
 			</ul>
-			<a href="contacto.php"><button class="btn btn-outline-success my-2 my-sm-0"
-					type="submit">Contacto</button></a>
+			<a href="contacto.php"><button class="btn btn-outline-success my-2 my-sm-0" type="submit">Contacto</button></a>
 		</div>
 	</nav>
-	<ul class="nav justify-content-center navbar navbar-dark bg-dark ">
 
-	</ul>
+
 </header>
 
 <body class="bg-dark">
+
+	<div class="container nvarTwo">
+		<div class="row">
+			<div class="my-3 col-12">
+				<ul class="nav justify navbar navbar-dark bg-secondary">
+					<li class="nav-item active">
+						<a class="nav-link nvarTwo " style="font-size: 25px;" href="hardware.php">Hardware</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link nvarTwo" style="font-size: 25px;" href="software.php">Software</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link nvarTwo" style="font-size: 25px;" href="#">Perifericos</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
